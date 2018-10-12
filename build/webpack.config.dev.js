6144ace9b3f1a88635649e8bc38c79c72405344e
Carlos Sanchez <carlos.sanchezr92@gmail.com>
2018-10-12 16:22:54 +0200

    add node dependencies

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,19 +1,49 @@
 'use strict'
+const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     mode: 'development',
     entry: [
-        './src/app.js'
+        './src/index.js'
     ],
+    devServer: {
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
+    },
     module: {
         rules: [
             {
                 test: /\.vue$/,
                 use: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
             }
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
     ]
 }
--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script>
     export default {
-        name: "App.vue"
+        name: 'App'
     }
 </script>
 
