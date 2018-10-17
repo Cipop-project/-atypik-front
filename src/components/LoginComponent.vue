8701cdd38b21fcbb92589e8f9c29089902334db7
Carlos Sanchez <carlos.sanchezr92@gmail.com>
2018-10-17 18:08:29 +0200

    improve login form

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div class="container-fluid">
     <div class="col-10 col-sm-6 col-md-4 col-lg-4 offset-1 offset-sm-3 offset-md-4 offset-lg-4 login-container">
       <form
@@ -36,9 +36,66 @@
       </form>
     </div>
   </div>
+</template>-->
+<template>
+  <v-container
+    fluid
+    fill-height>
+    <v-layout
+      align-center
+      justify-center>
+      <v-flex
+        xs12
+        sm8
+        md4>
+        <v-card
+          class="elevation-6">
+          <v-card-title>
+            <h3
+              class="text-xs-center">Connection</h3>
+          </v-card-title>
+          <v-card-text>
+            <v-form
+              ref="login_form"
+              v-model="valid">
+              <v-text-field
+                v-model="name"
+                label="Username or Email"
+                required/>
+              <v-text-field
+                v-model="password"
+                :type="'password'"
+                label="Mot de passe"
+                required/>
+              <v-btn
+                round
+                color="success"
+                @click="submit">Login</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      name: '',
+      password: '',
+      valid: true
+    }
+  },
+  methods: {
+    submit () {
+      if (this.$refs.login_form.validate()) {
+        // ok
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
