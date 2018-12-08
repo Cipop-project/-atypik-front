2c786fa7fa626722727cb912374f048f926b2121
Carlos Sanchez <carlos.sanchezr92@gmail.com>
2018-12-09 00:35:45 +0100

    search page items improvement

--- src/views/index/SearchView.vue
+++ src/views/index/SearchView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <v-container fluid>
+    <div class="spacer"/>
     <h1>Search</h1>
     <br>
     <v-layout
@@ -11,19 +12,27 @@
         md2
         xs4>
         <v-card>
-          <v-carousel>
+          <v-carousel
+            interval="8000"
+            class="card-img"
+            delimiter-icon="mdi-cisco-webex"
+            next-icon="mdi-chevron-right"
+            prev-icon="mdi-chevron-left">
             <v-carousel-item
               v-for="(image,i) in product.images"
               :key="i"
               :src="image.src"/>
           </v-carousel>
           <v-card-title primary-title>
-            <h3>product.title</h3>
+            <h3>{{ product.title }}</h3>
           </v-card-title>
+          <v-card-text>
+            {{ product.description }}
+          </v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -31,12 +40,24 @@ export default {
   name: 'SearchView',
   data () {
     return {
-      product: { 'title': 'first product', 'images': [{ 'title': 'first img', 'src': '/static/img/home_img_1.jpg' }, { 'title': 'second img', 'src': '/static/img/home_img_2.jpg' }] }
+      product: {
+        'title': 'first product',
+        'images': [
+          { 'title': 'first img', 'src': '/static/img/home_img_1.jpg' },
+          { 'title': 'second img', 'src': '/static/img/home_img_2.jpg' }
+        ],
+        'description': 'Here goes the description of first product'
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .card-img {
+    height: 200px;
+  }
+  .spacer {
+    margin-top: 64px;
+  }
 </style>
